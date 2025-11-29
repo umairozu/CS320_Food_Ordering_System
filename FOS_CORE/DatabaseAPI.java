fe86b546acd286345a2d85be38d491dda2239dd3
Mohamed khaled Becetti <mohamed.becetti@ozu.edu.tr>
2025-11-29 11:25:49 +0300

    error fix

--- FOS_CORE/DatabaseAPI.java
+++ FOS_CORE/DatabaseAPI.java
@@ -1,49 +1,61 @@
 package FOS_CORE;
 
 import FOS_DATA.UserData;
-
 import java.util.ArrayList;
 
 public class DatabaseAPI {
 
     private final UserData dao;
 
+
     public DatabaseAPI() {
-        this.dao = new FOS_DATA.UserData();
+        this.dao = new FOS_DATA.UserDataAccess();
     }
 
+    public boolean addNewCustomer(User user) {
 
-    public boolean saveRestaurantInfo(Restaurant restaurant) {
-        if (restaurant == null) return false;
-        return dao.saveRestaurantInfo(restaurant);
+
+        return dao.addNewCustomer(user);
     }
 
-    public ArrayList<Restaurant> getManagerRestaurants(Manager manager) {
-        return dao.getManagerRestaurants(manager);
+    public User getUserByEmail(String email) {
+        return dao.getUserByEmail(email);
     }
 
-    public ArrayList<Restaurant> getRestaurantByCity(String city) {
-        return dao.getRestaurantByCity(city);
+    public boolean changeUserPassword(User user, String newHashedPassword) {
+        return dao.changeUserPassword(user, newHashedPassword);
     }
 
-    public boolean saveMenuItem(MenuItem menuItem) {
-        return dao.saveMenuItem(menuItem);
+    public ArrayList<Card> fetchCustomerCards(Customer customer) {
+        return dao.fetchCustomerCards(customer);
     }
 
-    public ArrayList<MenuItem> getRestaurantMenuItems(Restaurant restaurant) {
-        return dao.getRestaurantMenuItems(restaurant);
+    public boolean addCardToCustomer(Customer customer, Card card) {
+        return dao.addCardToCustomer(customer, card);
     }
 
-    public ArrayList<Order> findCustomerOrders(Customer customer) {
-        return dao.findCustomerOrders(customer);
+    public ArrayList<Address> fetchCustomerAddresses(Customer customer) {
+        return dao.fetchCustomerAddresses(customer);
     }
 
-    public ArrayList<Order> findRestaurantOrders(Restaurant restaurant) {
-        return dao.findRestaurantOrders(restaurant);
+    public boolean addAddress(Customer customer, Address address) {
+        return dao.addAddress(customer, address);
     }
 
-    public boolean saveOrder(Order order) {
-        return dao.saveOrder(order);
+    public ArrayList<String> fetchCustomerPhoneNumbers(Customer customer) {
+        return dao.fetchCustomerPhoneNumbers(customer);
+    }
+
+    public boolean addPhoneNumber(Customer customer, String phoneNumber) {
+        return dao.addPhoneNumber(customer, phoneNumber);
+    }
+
+    public ArrayList<Order> fetchCustomerOrders(Customer customer, ArrayList<Restaurant> restaurants) {
+        return dao.fetchCustomerOrders(customer, restaurants);
+    }
+
+    public boolean insertCustomerOrder(Customer customer, Order order, Restaurant restaurant) {
+        return dao.insertCustomerOrder(customer, order, restaurant);
     }
 
 }
