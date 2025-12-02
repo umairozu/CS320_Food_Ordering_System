8482c5613ae1d8dd0311a933c00bb96108cb7fd9
Umair Ahmad <umair.ahmad@ozu.edu.tr>
2025-12-02 22:46:01 +0300

    Worked on ManagerDashboardWindow.java

--- FOS_UI/ManagerDashboardWindow.java
+++ FOS_UI/ManagerDashboardWindow.java
@@ -1,4 +1,17 @@
 package FOS_UI;
 
-public class ManagerDashboardWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class ManagerDashboardWindow extends JFrame {
+
+    public ManagerDashboardWindow() {
+        setTitle("Food Ordering System - Manager Dashboard");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Manager Dashboard", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+    }
 }
